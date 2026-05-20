26cc189af521301adc9dcbb73a858e3a3e8c4445
Michael Masterson <mgmastersonjr@gmail.com>
2026-05-20 12:45:14 -0300

    feat: adding completions

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,9 @@ mod npm;
 mod scaffold;
 
 use anyhow::Result;
-use clap::{Parser, Subcommand};
+use clap::{CommandFactory, Parser, Subcommand};
+use clap_complete::{Shell, generate};
+use std::io;
 
 #[derive(Parser)]
 #[command(
@@ -25,6 +27,12 @@ enum Commands {
     Generate(commands::generate::GenerateArgs),
     /// Check for and install updates
     Upgrade(commands::upgrade::UpgradeArgs),
+    /// Print shell completion script
+    Completions {
+        /// Shell to generate completions for
+        #[arg(value_enum)]
+        shell: Shell,
+    },
 }
 
 #[tokio::main]
@@ -39,5 +47,9 @@ async fn main() -> Result<()> {
             }
         },
         Commands::Upgrade(args) => commands::upgrade::run(args).await,
+        Commands::Completions { shell } => {
+            generate(shell, &mut Cli::command(), "m2s2", &mut io::stdout());
+            Ok(())
+        }
     }
 }
